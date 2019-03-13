aa4461f032f2c08b8c54b1e778441691bf2520da
Adam Hosman <github@adamsinbox.com>
2019-03-13 23:44:37 +0100

    Add basic meta-tests

---
+++ tester.js
@@ -0,0 +1,19 @@
+/**
+ * Example: sayThat('just wow') => 'that is just wow'
+ * @param that
+ * @returns {string}
+ */
+const sayThat = (that) => {
+  return `That is ${that}.`;
+};
+
+/**
+ * The unambigous constant value of the meaning of life
+ * @type {number}
+ */
+const MEANING_OF_LIFE = 42;
+
+module.exports = {
+  MEANING_OF_LIFE,
+  sayThat,
+};
