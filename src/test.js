d2260e7a940e563c6617a19dafb4fd0f1d8470ca
Adam Hosman <github@adamsinbox.com>
2019-03-14 00:51:35 +0100

    Merge test files in /src

--- src/test.js
+++ src/test.js
@@ -1,6 +1,14 @@
-const {MEANING_OF_LIFE, sayThat, Tile} = require("./tester");
+/* SAMPLE TESTS */
+
+const {MEANING_OF_LIFE, sayThat} = require("./testSamples");
 
 test("2 should be 2", () => expect(2).toBe(2));
 test("Imported constant should have proper value", () => expect(MEANING_OF_LIFE).toBe(42));
 test("Imported function should work", () => expect(sayThat('just wow')).toBe('That is just wow.'));
+
+
+/* THE REAL DEAL */
+
+const {Tile} = require('./classes');
+
 test("Imported Tile should be of type object", () => expect(typeof(new Tile())).toBe('object'));
