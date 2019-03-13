b2f88b877bba8487950718ef0213465cf557d170
Adam Hosman <github@adamsinbox.com>
2019-03-14 00:05:32 +0100

    Add to meta-tests

--- tester.js
+++ tester.js
@@ -13,7 +13,17 @@ const sayThat = (that) => {
  */
 const MEANING_OF_LIFE = 42;
 
+function Tile(selector, currentValue=null, wasJustMerged=false, wasJustSpawned=false, previousValueMvLen=null) {
+  this.currentValue = currentValue;
+  this.wasJustMerged = wasJustMerged;
+  this.wasJustSpawned = wasJustSpawned;
+  this.previousValueMvLen = previousValueMvLen;
+  this.selector = selector;
+}
+
+
 module.exports = {
   MEANING_OF_LIFE,
   sayThat,
+  Tile,
 };
--- tester.test.js
+++ tester.test.js
@@ -1,5 +1,6 @@
-const {MEANING_OF_LIFE, sayThat} = require("./tester");
+const {MEANING_OF_LIFE, sayThat, Tile} = require("./tester");
 
 test("2 should be 2", () => expect(2).toBe(2));
 test("Imported constant should have proper value", () => expect(MEANING_OF_LIFE).toBe(42));
 test("Imported function should work", () => expect(sayThat('just wow')).toBe('That is just wow.'));
+test("Imported Tile should be of type object", () => expect(typeof(new Tile())).toBe('object'));
