e6a4b4034050ddbbad2277059336a3abc63b7b9d
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-03-13 15:11:52 +0100

    Export functions from script.js

--- script.js
+++ script.js
@@ -258,3 +258,13 @@ document.addEventListener("keydown", listenForArrowPress);
 // console.log("Before squashRow: ", mockRowCopy);
 // squashRow(mockRow);
 // console.log("After squashRow: ", mockRow);
+
+
+module.exports = {
+  Tile,
+  createNextBoard,
+  squashBoard,
+  sliceMatrixPerDirection,
+  propagateTile,
+  attemptMerge,
+};
