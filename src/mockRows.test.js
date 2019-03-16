66a5c7ae8dce5cea3f598aedc87976b8ad9b310a
Adam Hosman <github@adamsinbox.com>
2019-03-16 14:26:42 +0100

    Add tests for propagateTile

--- src/mockRows.test.js
+++ src/mockRows.test.js
@@ -1,6 +1,22 @@
-const {squashRow, propagateTile, attemptMerge} = require('./BoardStaticMethods');
-const {mockRowPairs} = require('./mockRows');
+const {cloneDeep} = require('lodash');
 
+const {squashRow, propagateTile, attemptMerge} = require('./BoardStaticMethods');
+const {mockRowPairsForPropagateTileTest, mockRowPairsForSquashRowTest} = require('./mockRows');
+
+
+const propagateTileTestMutation = (mockRowPair) => {
+  let {indexFrom, propagatedProperly} = mockRowPair;
+  let original = cloneDeep(mockRowPair.original);
+  propagateTile(original, indexFrom);
+  expect(original).toEqual(propagatedProperly);
+};
+
+const propagateTileTestReturnValue = (mockRowPair) => {
+  let {indexFrom, expectedReturnValue} = mockRowPair;
+  let original = cloneDeep(mockRowPair.original);
+  let returnValue = propagateTile(original, indexFrom);
+  expect(returnValue).toEqual(expectedReturnValue);
+};
 
 const squashRowTest = (mockRowPair) => {
   squashRow(mockRowPair.original);
@@ -8,46 +24,74 @@ const squashRowTest = (mockRowPair) => {
 };
 
 
-describe('squashRow()', () => {
+describe('attemptMerge()', () => {
 
-  test("Should not move tile from end of row", () =>
-      squashRowTest(mockRowPairs['0,0,0,2'])
-  );
-
-  test("Should move tile towards end of row", () =>
-      squashRowTest(mockRowPairs['0,0,2,0'])
-  );
-
-  test("Should not merge tiles of non-identical value", () =>
-      squashRowTest(mockRowPairs['0,0,4,2'])
-  );
-
-  test("Should stop tile movement on meeting tile of differing value", () =>
-      squashRowTest(mockRowPairs['4,0,0,2'])
-  );
-
-  test("Should merge two neighboring tiles of identical value", () =>
-      squashRowTest(mockRowPairs['0,0,2,2'])
-  );
-
-  test("Should merge two non-neighboring but mutually visible tiles of identical value" , () =>
-      squashRowTest(mockRowPairs['2,0,0,2'])
-  );
-
-  test("Should merge two pairs of values in same move" , () =>
-      squashRowTest(mockRowPairs['2,2,4,4'])
-  );
-
-  test("Should not merge tile into one that's already been merged" , () =>
-      squashRowTest(mockRowPairs['4,0,2,2'])
-  );
+  // TODO
 
 });
 
 
-// TODO
-// describe('propagateTile()', () => {});
+describe('propagateTile()', () => {
+
+  test("Should not move tile from end of row", () => {
+    propagateTileTestMutation(mockRowPairsForPropagateTileTest["0,0,0,2 indexFrom=3"])
+  });
+
+  test("Should move tile towards end of row", () => {
+    propagateTileTestMutation(mockRowPairsForPropagateTileTest["2,0,0,0 indexFrom=0"])
+  });
+
+  test("Should stop tile movement on meeting another tile of any value", () => {
+    propagateTileTestMutation(mockRowPairsForPropagateTileTest["2,0,0,2 indexFrom=0"])
+  });
+
+  test("Should return index where tile has been moved", () => {
+    propagateTileTestReturnValue(mockRowPairsForPropagateTileTest["0,0,0,2 indexFrom=3"]);
+  });
+
+  test("Should return index where tile has been moved", () => {
+    propagateTileTestReturnValue(mockRowPairsForPropagateTileTest["2,0,0,0 indexFrom=0"]);
+  });
+
+  test("Should return index where tile has been moved", () => {
+    propagateTileTestReturnValue(mockRowPairsForPropagateTileTest["2,0,0,2 indexFrom=0"]);
+  });
+
+});
 
 
-// TODO
-// describe('attemptMerge()', () => {});
+describe('squashRow()', () => {
+
+  test("Should not move tile from end of row", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['0,0,0,2'])
+  );
+
+  test("Should move tile towards end of row", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['0,0,2,0'])
+  );
+
+  test("Should not merge tiles of non-identical value", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['0,0,4,2'])
+  );
+
+  test("Should stop tile movement on meeting tile of differing value", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['4,0,0,2'])
+  );
+
+  test("Should merge two neighboring tiles of identical value", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['0,0,2,2'])
+  );
+
+  test("Should merge two non-neighboring but mutually visible tiles of identical value", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['2,0,0,2'])
+  );
+
+  test("Should merge two pairs of values in same move", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['2,2,4,4'])
+  );
+
+  test("Should not merge tile into one that's already been merged", () =>
+      squashRowTest(mockRowPairsForSquashRowTest['4,0,2,2'])
+  );
+
+});
