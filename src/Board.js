335d6c2d298d01a8e59b85a2361687cc5ceb124d
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-03-19 11:35:51 +0100

    Replace table creation with table import in Board

--- src/Board.js
+++ src/Board.js
@@ -7,15 +7,9 @@ const {mockList} = require("./mockBoards");
  * Create new Board object.
  * @constructor
  */
-function Board() {
+function Board(scenario="noMock") {
   this.hasChanged = false;
-  this.matrix = [];
-  for (let row = 0; row < 4; row++) {
-    this.matrix[row] = [];
-    for (let column = 0; column < 4; column++) {
-      this.matrix[row].push(new Tile(`#r${row}c${column}`));
-    }
-  }
+  this.matrix = mockList[scenario];
   /**
    * Add new tile(s) to the board.
    * @param {number} howMany - How many tiles to add to the board.
