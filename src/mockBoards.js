6d3702d46668c1c69b8bff1b20e9ebe34c17892f
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-03-19 11:35:14 +0100

    Add default values to mockList (noMock)

--- src/mockBoards.js
+++ src/mockBoards.js
@@ -1,7 +1,11 @@
 const {Tile} = require("./Tile.js");
 
 const mockList = {
-  "noMock": false,
+  "noMock": [
+    [new Tile("#r0c0"),    new Tile("#r0c1"),    new Tile("#r0c2"),   new Tile("#r0c3")],
+    [new Tile("#r1c0"),   new Tile("#r1c1"),  new Tile("#r1c2"),   new Tile("#r1c3")],
+    [new Tile("#r2c0"),    new Tile("#r2c1"),   new Tile("#r2c2"),  new Tile("#r2c3")],
+    [new Tile("#r3c0"),    new Tile("#r3c1"),    new Tile("#r3c2"),   new Tile("#r3c3")]],
   "almostLost": [
       [new Tile("#r0c0", 2),    new Tile("#r0c1", 8),    new Tile("#r0c2", 32),   new Tile("#r0c3", 2)],
       [new Tile("#r1c0", 16),   new Tile("#r1c1", 128),  new Tile("#r1c2", 64),   new Tile("#r1c3", 8)],
