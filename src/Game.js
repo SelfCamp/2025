95078d9fb9796e17ce7207046e30da519b472836
Adam Hosman <github@adamsinbox.com>
2019-03-20 17:56:51 +0100

    Draft Game class

---
+++ src/Game.js
@@ -0,0 +1,20 @@
+const {Board} = require('./Board');
+
+
+/**
+ * Create new Game object
+ *
+ * - Contains all state required to play 1 game and move in its history
+ *
+ * @param difficulty
+ * @constructor
+ */
+function Game(difficulty=1) {
+  this.difficulty = difficulty;
+  this.score = 0;
+  this.gameTimeline = [new Board()];
+  this.head = 0;
+
+  // TODO: Implement
+
+}
