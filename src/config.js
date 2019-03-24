1d01ce0353bec159195c1cbd3c3fc441a0e545f7
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-03-24 21:57:15 +0100

    Add ANIMATION_NEEDED switch to config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 const ANIMATION_SLIDE_DURATION = 400;  // ms
 const ARROW_PRESS_TIMEOUT = ANIMATION_SLIDE_DURATION;
 const MOCK_SCENARIO = 'noMock';  // {"noMock"|"almostLost"|"almostWon"|"oneMissing"}
+const ANIMATION_NEEDED = false;
 
 
 module.exports = {
   ARROW_PRESS_TIMEOUT,
   ANIMATION_SLIDE_DURATION,
-  MOCK_SCENARIO
+  MOCK_SCENARIO,
+  ANIMATION_NEEDED
 };
