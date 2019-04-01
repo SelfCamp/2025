140005c3ee58ceaaca9c29924b2441e08cd0a470
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-04-01 23:34:47 +0200

    Add event to rules and about button

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@
 
 const {Game} = require('./classes/Game');
 const {listenForKeyPress, handleSliderChange} = require('./eventHandling');
-const {applyConfigToDOM, updateView, updateTimerInDOM} = require('./domManipulation');
+const {applyConfigToDOM, updateView, updateTimerInDOM, displayAbout, displayRules} = require('./domManipulation');
 const config = require('./config');
 
 
@@ -14,6 +14,8 @@ document.addEventListener("keydown", (event) => listenForKeyPress(game, event));
 let slider = document.querySelector("#game-history-slider");
 slider.addEventListener("change", (event) => handleSliderChange(game,event));
 window.setInterval(() => updateTimerInDOM(game.elapsedTimeInSeconds()), 1000);
+document.querySelector("#rules-button").addEventListener("click", () => displayRules());
+document.querySelector("#about-button").addEventListener("click", () => displayAbout());
 
 updateView(
     game.currentBoard(),
